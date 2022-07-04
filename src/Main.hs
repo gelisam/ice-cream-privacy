34cf3bb17a9a3cefe4c973e7875f6c961632a006
Samuel Gélineau <gelisam+github@gmail.com>
2022-07-04 10:00:04 -0400

    way too many possiblities for brute-force

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,26 @@
 module Main where
 
 
+data Gender
+  = Male
+  | Female
+  deriving (Eq, Ord, Show)
+
+data PersonInfo = PersonInfo
+  { age
+      :: Int
+  , gender
+      :: Gender
+  , likesIceCream
+      :: Bool
+  , isMarried
+      :: Bool
+  }
+  deriving Show
+
+
 main
   :: IO ()
 main = do
+  print $ (800 :: Integer) ^ (7 :: Integer)
   putStrLn "typechecks."
